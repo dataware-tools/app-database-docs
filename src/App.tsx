5ebbbb5e89ee6cc6697f3e310238575af3b1ae58
Daiki Hayashi <hayashi.daiki@nagoya-u.jp>
2020-10-04 16:49:18 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import { PageWrapper } from "./PageWrapper";
+import Sample from "./components/Sample";
+
+const App = () => {
+  return (
+    <PageWrapper>
+      <Sample />
+    </PageWrapper>
+  );
+};
+
+export default App;
